b0bea157bb39bd8b1fdd478e6e8df88e1f4733f1
WTC13 <wendell.timoteoc@gmail.com>
2021-10-19 23:47:49 -0300

    criar a comida

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake [0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -16,9 +20,14 @@ function criarBG(){
 
 function criarCobrinha(){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "greenstyle";
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -26,8 +35,8 @@ document.addEventListener("keydown", update);
 function update(event){
     if(event.keyCode == 37 && direction != "right") direction = "left"
     if(event.keyCode == 38 && direction != "down") direction = "up"
-    if(event.keyCode == 39 && direction != "up") direction = "down"
-    if(event.keyCode == 40 && direction != "right") direction = "left"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
 }
 
 function iniciarJogo(){
@@ -39,6 +48,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
